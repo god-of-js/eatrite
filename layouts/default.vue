0c730381893f7fdddf6ab649bac3b9449f671f4a
god-of-js <henryeze019@gmail.com>
2023-11-05 21:01:13 +0100

    chore(): default layout set

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <slot />
+        <p>default layout</p>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, perferendis! Consequatur, nulla nesciunt. Voluptates fugit, minima porro reiciendis, nulla corrupti a amet explicabo illo quis eos dolore aut esse. Culpa.
+    </div>
+</template>
+
+<style scoped>
+
+</style>
